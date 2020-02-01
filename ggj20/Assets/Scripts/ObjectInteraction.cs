bf4eda0435960f396e56b788e60f1af90da5710e
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2020-02-01 21:00:52 +0100

    id #8 implement proximity logic

--- ggj20/Assets/Scripts/ObjectInteraction.cs
+++ ggj20/Assets/Scripts/ObjectInteraction.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace com.BrutalHack.GlobalGameJam20
 {
     [RequireComponent(typeof(Cinematic))]
     public class ObjectInteraction : MonoBehaviour
     {
+        public ObjectEnum objectEnum;
         public bool done;
         private Cinematic _cinematic;
         private InteractionManager _interactionManager;
@@ -14,6 +14,16 @@ namespace com.BrutalHack.GlobalGameJam20
         {
             _cinematic = GetComponent<Cinematic>();
             _interactionManager = GameObject.FindWithTag("InteractionManager").GetComponent<InteractionManager>();
+            var child = GetComponentInChildren<ObjectProximity>();
+            if (child != null)
+            {
+                child.objectEnum = objectEnum;
+            }
+            else
+            {
+                Debug.LogError(
+                    $"{nameof(ObjectInteraction)} {objectEnum} has no child with component {typeof(ObjectProximity)}");
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D other)
---
+++ ggj20/Assets/Scripts/ObjectProximity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace com.BrutalHack.GlobalGameJam20
+{
+    public class ObjectProximity : MonoBehaviour
+    {
+        public ObjectEnum objectEnum;
+
+        private void OnTriggerStay(Collider other)
+        {
+            var distance = Vector3.Distance(other.transform.position, transform.position);
+            Debug.Log($"{objectEnum} {distance}");
+        }
+    }
+}
