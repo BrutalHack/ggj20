fe0dfdf6663bd892fbe6fde123f0882ed003b6e4
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2020-02-01 16:14:10 +0100

    id #2 add objecs and collision

--- ggj20/Assets/Scripts/PlayerMovement.cs
+++ ggj20/Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,22 @@
 namespace com.BrutalHack.GlobalGameJam20
 {
     [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(SpriteRenderer))]
     public class PlayerMovement : MonoBehaviour
     {
         [Range(1, 10)] [SerializeField] private int speed = 4;
         [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;
 
-        private PlayerInput _playerInput;
         private Rigidbody2D _rigidBody2D;
+        private SpriteRenderer _spriteRenderer;
+        private PlayerInput _playerInput;
         private Vector2 _movementInput;
         private Vector3 velocity = Vector3.zero;
 
         private void Awake()
         {
             _rigidBody2D = GetComponent<Rigidbody2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
             _playerInput = new PlayerInput();
             _playerInput.PlayerControls.Movement.performed += ctx => _movementInput = ctx.ReadValue<Vector2>();
         }
@@ -28,6 +31,14 @@ namespace com.BrutalHack.GlobalGameJam20
             Vector3 targetVelocity = new Vector2(moveX, moveY);
             _rigidBody2D.velocity =
                 Vector3.SmoothDamp(_rigidBody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
+            
+            if (moveX > 0)
+            {
+                _spriteRenderer.flipX = true;
+            }else if (moveX < 0)
+            {
+                _spriteRenderer.flipX = false;
+            }
         }
 
         private void OnEnable()
