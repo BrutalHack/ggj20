638a3443c745029033334d59661861085618adbf
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2020-02-02 16:05:53 +0100

    added content to title scene

---
+++ ggj20/Assets/Scripts/TitleManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace com.BrutalHack.GlobalGameJam20
+{
+    public class TitleManager : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            Debug.Log("Start Game");
+            SceneManager.LoadScene("Game");
+        }
+
+        public void ExitGame()
+        {
+            Debug.Log("Exit Game");
+            Application.Quit();
+        }
+    }
+}
